2dc6856b2cb09051ab7e4ac1d8806b6d7857aa18
김경호 <gyeonghokim@gimgyeonghoui-MacBookAir.local>
2021-05-16 21:23:57 +0900

    ADD REGISTER ROUTE

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const mongoURI = require('./dev');
+const bodyParser = require("body-parser");
+const mongoURI = require("./dev");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-mongoose.connect(
-  mongoURI.mongoURI,
-  {
+mongoose
+  .connect(mongoURI.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  }
-).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입시 필요한 정보들을 클라에서 가져오면 데이터베이스에 넣음
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    })
+  });
 });
 
 app.listen(port, () => {
